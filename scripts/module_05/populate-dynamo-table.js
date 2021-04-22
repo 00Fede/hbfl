2ac889cbf8ae4660c40288e9517e938bf994d58d
Federico Ocampo <feoorti@bancolombia.com.co>
2021-04-21 21:38:28 -0500

    dell version

--- scripts/module_05/populate-dynamo-table.js
+++ scripts/module_05/populate-dynamo-table.js
@@ -2,21 +2,36 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Declare dynamoDB DocumentClient object
+const client = new AWS.DynamoDB.DocumentClient()
 
 helpers.getHamsterData()
-.then(data => populateTable('hamsters', data))
-.then(() => helpers.getRaceData())
-.then(data => populateTable('races', data))
-.then(data => console.log(data))
+  .then(data => populateTable('hamsters', data))
+  .then(() => helpers.getRaceData())
+  .then(data => populateTable('races', data))
+  .then(data => console.log(data))
 
-function populateTable (tableName, data) {
-  // TODO: Create params const object
+function populateTable(tableName, data) {
+  const params = {
+    RequestItems: {
+      [tableName]: data.map((item) => {
+        return {
+          PutRequest: {
+            Item: item
+          }
+        }
+      })
+    }
+  }
+
+  console.log(params)
 
   return new Promise((resolve, reject) => {
-    // TODO: Call batch write function
+    client.batchWrite(params, (err, data) => {
+      if(err) reject(err)
+      else resolve(data)
+    })
   })
 }
